739a439bec01ecb8bf9b7a670647c3bfe4bcb9ab
Nightdavisao <nightdavisao27@protonmail.com>
2020-10-24 16:01:02 -0300

    Remove "not-needed to-do" in the code

--- loritta-plugins/mal-commands/src/main/kotlin/net/perfectdreams/loritta/plugin/malcommands/utils/Extensions.kt
+++ loritta-plugins/mal-commands/src/main/kotlin/net/perfectdreams/loritta/plugin/malcommands/utils/Extensions.kt
@@ -5,18 +5,15 @@ import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import org.jsoup.safety.Whitelist
 
-// TODO: move this to out of there
 fun Document.findElementByString(element: String, string: String): Element? {
     return this.selectFirst("$element:contains($string)")
 }
 
-// TODO: move this to out of there
 fun Element.nextSiblingString(): String? {
     return this.nextSibling().toString().removeHtmlEntities()
             ?.trim()
 }
 
-// TODO: move this to out of there
 fun Element.preservedText(): String? {
     val document = this
     document.select("br").before("\\n")
@@ -24,7 +21,6 @@ fun Element.preservedText(): String? {
     return document.html().replace("\\n", "\n").removeHtmlEntities()
 }
 
-// TODO: move this to out of there
 fun String.removeHtmlEntities(): String? {
     return Jsoup.clean(
             this,
